dd601ae30e8998e54f4acb69b89fb6964a8cffd2
George Dewhirst <dewhirst.george@gmail.com>
2021-11-14 00:10:39 +1100

    init commit

---
+++ src/components/navbar.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+    return (
+        <div>
+            <ul>
+                <li>
+                    <Link to='/'>Home</Link>
+                </li>
+                <li>
+                    <Link to='/articles'></Link>
+                </li>
+                <li>
+                    <Link to='/about'></Link>
+                </li>
+            </ul>
+        </div>
+    )
+}
