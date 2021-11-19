8a74db21ec6a208d583ce8ffedc0974fdb828aa1
George Dewhirst <dewhirst.george@gmail.com>
2021-11-19 21:52:06 +1100

    george discovers flex box - need to make it mobile friendly

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,19 +1,32 @@
 import {  Link  } from "react-router-dom";
 
+const pages = [
+    {
+        path: "/",
+        page: "Home"
+    },
+    {
+        path: "/archive",
+        page: "Archive"
+    },
+    {
+        path: "/about",
+        page: "About"
+    }
+]
+
 export default function Navbar() {
     return (
-        <div>
-            <ul>
-                <li>
-                    <Link to='/'>Home</Link>
-                </li>
-                <li>
-                    <Link to='/archive'>Archive</Link>
-                </li>
-                <li>
-                    <Link to='/about'>About</Link>
-                </li>
-            </ul>
+        <div style={{display: "flex", flexDirection: "row", justifyContent: "space-between"}}>
+            <div style={{display: "flex", flexDirection: "row", justifyContent: "flex-start"}}>
+                {pages.map((page) => {
+                    return (
+                        <Link to={page.path} style={{flex: "0 0 6rem", color: "white"}}>
+                            <h2>{page.page}</h2>
+                        </Link>
+                    )
+                })}
+            </div>
             <div>
                 <h2>Geo</h2>
             </div>
