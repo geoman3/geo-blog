289028944e8021974e1e3b6f7c9d0c53d2f6186a
George Dewhirst <dewhirst.george@gmail.com>
2021-11-26 16:49:32 +1100

    udner construction

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ import "./footer.css"
 export default function Footer() {
     return (
         <footer className="Footer">
-
+            <h3>Site is currently under construction</h3>
         </footer>
     )
 }
